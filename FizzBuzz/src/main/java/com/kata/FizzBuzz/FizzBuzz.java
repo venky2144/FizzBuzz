ba330bde47765a64307bb77fd134605b98f5b49e
Venkat <Venkat@Venkat-PC>
2021-04-01 08:38:49 +0200

    Fizz Logic has added

--- FizzBuzz/src/main/java/com/kata/FizzBuzz/FizzBuzz.java
+++ FizzBuzz/src/main/java/com/kata/FizzBuzz/FizzBuzz.java
@@ -2,9 +2,12 @@ package com.kata.FizzBuzz;
 
 public class FizzBuzz {
 
-	public static String getResult(int i) {
+	public static String getResult(int inputValue) {
+		if (inputValue % 3 == 0) 
+			return "fizz";
+		return Integer.toString(inputValue);
 		
-		return "test";
+		
 	}
 
 }
