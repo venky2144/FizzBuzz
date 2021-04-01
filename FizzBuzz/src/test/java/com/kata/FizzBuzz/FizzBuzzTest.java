31814264f2a62aeef5472b7a900409bd77e5270a
Venkat <Venkat@Venkat-PC>
2021-04-01 08:28:20 +0200

    Test Scenario for Fizz

--- FizzBuzz/src/test/java/com/kata/FizzBuzz/FizzBuzzTest.java
+++ FizzBuzz/src/test/java/com/kata/FizzBuzz/FizzBuzzTest.java
@@ -1,5 +1,12 @@
 package com.kata.FizzBuzz;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class FizzBuzzTest {
 
+	 @Test
+	    public void getResultShouldReturnFizzIfTheNumberIsDividableBy3() {
+	        Assert.assertEquals("fizz", FizzBuzz.getResult(3));
+	    }
 }
