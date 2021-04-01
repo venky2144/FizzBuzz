fb40fb2a1ba702441435fe241515e5f86188d004
Venkat <Venkat@Venkat-PC>
2021-04-01 08:49:47 +0200

    Code added for buzz

--- FizzBuzz/src/main/java/com/kata/FizzBuzz/FizzBuzz.java
+++ FizzBuzz/src/main/java/com/kata/FizzBuzz/FizzBuzz.java
@@ -5,8 +5,9 @@ public class FizzBuzz {
 	public static String getResult(int inputValue) {
 		if (inputValue % 3 == 0) 
 			return "fizz";
-		return Integer.toString(inputValue);
-		
+		 if(inputValue % 5 == 0)
+			 return "buzz";
+		 return Integer.toString(inputValue);
 		
 	}
 
