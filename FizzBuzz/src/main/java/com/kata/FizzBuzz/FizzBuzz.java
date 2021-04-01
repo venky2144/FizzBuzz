717604550c5e23cc1f2be3a6b54f1cab55e81688
Venkat <Venkat@Venkat-PC>
2021-04-01 09:05:24 +0200

    changes made to business logic

--- FizzBuzz/src/main/java/com/kata/FizzBuzz/FizzBuzz.java
+++ FizzBuzz/src/main/java/com/kata/FizzBuzz/FizzBuzz.java
@@ -3,12 +3,14 @@ package com.kata.FizzBuzz;
 public class FizzBuzz {
 
 	public static String getResult(int inputValue) {
+		
+		if (inputValue % 15 == 0)
+			 return "fizzbuzz";
 		if (inputValue % 3 == 0) 
 			return "fizz";
 		 if(inputValue % 5 == 0)
 			 return "buzz";
-		 if (inputValue % 15 == 0)
-			 return "fizzbuzz";
+		 
 		 return Integer.toString(inputValue);
 		
 	}
